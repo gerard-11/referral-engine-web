1711ee7be05e288b519d540c1780b48688f3f457
Gerardo J. Rito <grrdrito@gmail.com>
2026-04-28 12:41:51 -0600

    feat(init): bootstrap referral-engine-web with vite react typescript router

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import {Route, Routes} from "react-router-dom";
+
+function App() {
+
+  return (
+    <Routes>
+      <Route path="/" element={<h1>Home</h1>} />
+      <Route path="/login" element={<h1>Login</h1>} />
+      <Route path="/dashboard" element={<h1>Dashboard</h1>} />
+    </Routes>
+  )
+}
+
+export default App
