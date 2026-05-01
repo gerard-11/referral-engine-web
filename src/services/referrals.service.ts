100ebc41beffbcc6a53b7e07d8ff89fea6502b55
Gerardo J. Rito <grrdrito@gmail.com>
2026-04-30 18:40:36 -0600

    feat(Sidebar-ProfilePage-Footer): add Sidebar, ProfilePage and Footer pages

---
+++ src/services/referrals.service.ts
@@ -0,0 +1,8 @@
+import type { ReferralResponse} from "../../shared/types/types.ts";
+import {api} from "../../shared/services/api";
+
+export const getReferrals = async (referrerId:string, page = 1):Promise <ReferralResponse> => {
+    return api.get(`${referrerId}/referrals/`, {
+        params: { page },
+    });
+};
